54fd9a3346bf813d7ae4590401c9f2508273126c
bruno <bruno.tanaka@e-deploy.com.br>
2021-04-14 13:43:32 -0400

    loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { AppProps } from 'next/app'
 import { ReactElement } from 'react'
+import useLoading from '../hooks/useLoading'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  useLoading()
   return <Component {...pageProps} />
 }
 
